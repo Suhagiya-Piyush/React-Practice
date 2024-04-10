1015db3a90a9aebc19e8946c2ddcc0b3587c83d7
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2024-04-10 09:44:50 +0530

    first commit

---
+++ Lacture-2/first_component/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import First_Component from "./Components/First_Component";
+
+function App() {
+  return (
+    <div className="App">
+      <First_Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lacture-2/first_component/src/Components/First_Component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const First_Component = () => {
+  return (<>
+    <div>My First_Component</div>
+    <h1>This is My First Component</h1>
+
+    {/* React-JS in Details */}
+    <h3>What Is React?</h3>
+    <p>React is a framework that employs Webpack to automatically compile React, JSX, and ES6 code while handling CSS file prefixes. React is a JavaScript-based UI development library. Although React is a library rather than a language, it is widely used in web development. The library first appeared in May 2013 and is now one of the most commonly used frontend libraries for web development. React offers various extensions for entire application architectural support, such as Flux and React Native, beyond mere UI.</p>
+
+    <h3>ReactJS History</h3>
+    <p>When compared to other technologies on the market, React is a new technology. Jordan Walke, a software engineer at Facebook, founded the library in 2011, giving it life. The likes of XHP, a straightforward HTML component framework for PHP, have an influence on React. React's newsfeed was its debut application in 2011. Later, Instagram picks it up and incorporates it into their platform.</p>
+    </>
+  )
+}
+
+export default First_Component;
